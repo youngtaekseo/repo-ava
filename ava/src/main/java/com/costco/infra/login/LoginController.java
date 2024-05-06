063af5fd7451e825c82114d65020791c044f38d0
ramses <ramses69@naver.com>
2024-05-06 12:39:16 +0900

    환경변수에서 이메일정보 읽도록 수정

--- ava/src/main/java/com/costco/infra/login/LoginController.java
+++ ava/src/main/java/com/costco/infra/login/LoginController.java
@@ -1,20 +1,25 @@
 package com.costco.infra.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.costco.common.constants.Constants;
 
 @Controller
 public class LoginController {
-
+	@Value("${my_email}")
+	private String myEmail;
+	
 	@Autowired
 	LoginService service;
 	
 	// 관리자 로그인
 	@RequestMapping(value = "/loginCostco")
-	public String loginCostco() throws Exception {
+	public String loginCostco(Model model) throws Exception {
+		model.addAttribute("myEmail", myEmail);
 		return Constants.PATH_LOGIN + "loginCostco";
 	}	
 }
